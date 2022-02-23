b684867cd057fc12be1e1778f9897c3b82f17a6e
onepopcorn <marc.feliu@gmail.com>
2022-02-23 16:09:52 -0700

    chore: created testing setup

--- src/setupTests.js
+++ src/setupTests.js
@@ -3,3 +3,20 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+
+
+// Testing mocks for Shoelace
+// for more info, visit: https://shoelace.style/frameworks/react?id=testing-with-jest
+Object.defineProperty(window, 'matchMedia', {
+    writable: true,
+    value: jest.fn().mockImplementation(query => ({
+        matches: false,
+        media: query,
+        onchange: null,
+        addListener: jest.fn(), // deprecated
+        removeListener: jest.fn(), // deprecated
+        addEventListener: jest.fn(),
+        removeEventListener: jest.fn(),
+        dispatchEvent: jest.fn(),
+    })),
+});
