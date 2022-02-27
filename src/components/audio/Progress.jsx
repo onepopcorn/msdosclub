9b062e7f55e508b6c52f3b5379a5f7b2b5342934
onepopcorn <marc.feliu@gmail.com>
2022-02-26 17:11:26 -0700

    fix: handled window resize to update track position accordingly

--- src/components/audio/Progress.jsx
+++ src/components/audio/Progress.jsx
@@ -13,8 +13,7 @@ const eventToValue = (e, ref) => {
     // Get clientX depdending on the event type. Defaults to click event type
     let { clientX: value } = e
     if ('touches' in e && e.touches.length > 0) value = e.touches[0].clientX
-    if ('changedTouches' in e && e.changedTouches.length > 0)
-        value = e.changedTouches[0].clientX
+    if ('changedTouches' in e && e.changedTouches.length > 0) value = e.changedTouches[0].clientX
 
     return clamp(0, value - offsetX, total)
 }
@@ -22,19 +21,11 @@ const eventToValue = (e, ref) => {
 // Utility function to calculate value to be passed to onChange
 const calculateChangeValue = (e, ref, total) => {
     const xPos = eventToValue(e, ref)
-    const newPercent = valueToPercent(
-        xPos,
-        ref.current.getBoundingClientRect().width,
-    )
+    const newPercent = valueToPercent(xPos, ref.current.getBoundingClientRect().width)
     return percentToValue(newPercent, total)
 }
 
-export default function Progress({
-    value = 0,
-    max,
-    onChange = () => null,
-    loading = false,
-}) {
+export default function Progress({ value = 0, max, onChange = () => null, loading = false }) {
     const [dragging, setDragging] = useState(false)
     const [position, setPosition] = useState(0)
     const trackRef = useRef(null)
@@ -48,14 +39,20 @@ export default function Progress({
         setPosition(eventToValue(e, trackRef))
     }
 
-    // Set thumb at initial position
+    // Set thumb at initial position & handle resize
     useEffect(() => {
         if (dragging) return
-        const value = percentToValue(
-            percent,
-            trackRef.current.getBoundingClientRect().width,
-        )
-        setPosition(value)
+
+        const calculatePosition = () => {
+            const value = percentToValue(percent, trackRef.current.getBoundingClientRect().width)
+            setPosition(value)
+        }
+        calculatePosition()
+        window.addEventListener('resize', calculatePosition)
+
+        return () => {
+            window.removeEventListener('resize', calculatePosition)
+        }
         // Skip dragging as dependency to prevent update of position until the next rerender
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [percent])
