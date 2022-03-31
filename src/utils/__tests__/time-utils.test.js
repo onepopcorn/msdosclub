0c8c31662c886be9101713532a7cbcccc7f81646
onepopcorn <marc.feliu@gmail.com>
2022-03-31 15:52:22 -0600

    feat(time-utils): added HMSToSec time utility function

--- src/utils/__tests__/time-utils.test.js
+++ src/utils/__tests__/time-utils.test.js
@@ -1,14 +1,31 @@
-import { secToHMS } from '../time-utils'
+import { secToHMS, HMSToSec } from '../time-utils'
 
-it('secToHMS should return formatted correct time', () => {
+test('secToHMS should return formatted correct time', () => {
     expect(secToHMS(8936)).toBe('02:28:56')
     expect(secToHMS(3456)).toBe('00:57:36')
     expect(secToHMS(1)).toBe('00:00:01')
 })
 
-it('secToHMS should throw if anything but a number is passed to the function', () => {
+test('secToHMS should throw if anything but a number is passed to the function', () => {
     expect(() => secToHMS('invalid type')).toThrow('Provided time must be a number')
     expect(() => secToHMS('0')).toThrow('Provided time must be a number')
     expect(() => secToHMS(NaN)).toThrow('Provided time must be a number')
     expect(() => secToHMS(Infinity)).toThrow('Provided time must be a number')
+})
+
+test('HMSToSec should return total seconds from a formatted time string', () => {
+    expect(HMSToSec('00:00:00')).toBe(0)
+    expect(HMSToSec('00:00:01')).toBe(1)
+    expect(HMSToSec('00:03:00')).toBe(180)
+    expect(HMSToSec('02:00:03')).toBe(7203)
+    expect(HMSToSec('23:59:59')).toBe(86399)
+})
+
+test('HMSToSec should throw if an invalid time string is passed', () => {
+    expect(() => HMSToSec('25:00:00')).toThrow('Provided string is not a valid HMS')
+    expect(() => HMSToSec('-01:00:00')).toThrow('Provided string is not a valid HMS')
+    expect(() => HMSToSec('00:60:00')).toThrow('Provided string is not a valid HMS')
+    expect(() => HMSToSec('00:100:00')).toThrow('Provided string is not a valid HMS')
+    expect(() => HMSToSec('00:00:70')).toThrow('Provided string is not a valid HMS')
+    expect(() => HMSToSec('24:00:00')).toThrow('Provided string is not a valid HMS')
 })
--- src/utils/time-utils.js
+++ src/utils/time-utils.js
@@ -2,7 +2,14 @@ export function secToHMS(time) {
     if (!Number.isFinite(time)) throw TypeError('Provided time must be a number')
 
     const hours = Math.floor(time / 3600)
-    const min = Math.floor(time % 3600 / 60)
-    const sec = Math.floor(time % 3600 % 60)
+    const min = Math.floor((time % 3600) / 60)
+    const sec = Math.floor((time % 3600) % 60)
     return `${hours.toString().padStart(2, '0')}:${min.toString().padStart(2, '0')}:${sec.toString().padStart(2, '0')}`
+}
+
+export function HMSToSec(hms) {
+    if (!hms.match(/^[0-2][0-3]:[0-5][0-9]:[0-5][0-9]$/gi)) throw TypeError('Provided string is not a valid HMS')
+
+    const [hours, minutes, seconds] = hms.split(':')
+    return parseInt(hours) * 60 * 60 + parseInt(minutes) * 60 + parseInt(seconds)
 }
