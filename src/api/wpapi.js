8c4302e497e681581b53a6196834f296a9b2f388
onepopcorn <marc.feliu@gmail.com>
2022-03-31 16:35:43 -0600

    refactor(api): removed unsued parameters

--- src/api/wpapi.js
+++ src/api/wpapi.js
@@ -4,16 +4,10 @@ import { processPosts } from 'utils/wp-utils.ts'
 const endpoint = 'https://msdos.club/wp-json'
 const wp = new WPAPI({ endpoint })
 
-export const getPosts = async ({ queryKey, pageParam = 1, perNextPage }) => {
+export const getPosts = async ({ queryKey, pageParam = 1 }) => {
     // eslint-disable-next-line no-unused-vars
     const [_, { categories = 2, perPage = 6 }] = queryKey
-    const data = await wp
-        .posts()
-        .categories(categories.toString())
-        .perPage(perNextPage || perPage)
-        .page(pageParam)
-        .embed()
-        .get()
+    const data = await wp.posts().categories(categories.toString()).perPage(perPage).page(pageParam).embed().get()
     return {
         posts: processPosts(data),
         nextPage: data._paging.totalPages > pageParam ? pageParam + 1 : null,
