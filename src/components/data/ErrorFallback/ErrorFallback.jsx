58205c79da0f146affbe96d5f3fb490636d416d6
onepopcorn <marc.feliu@gmail.com>
2022-04-12 14:48:53 -0600

    feat(error): created an error fallback component with a retry button for when there's an error while loding posts or other content

---
+++ src/components/data/ErrorFallback/ErrorFallback.jsx
@@ -0,0 +1,22 @@
+import PropTypes from 'prop-types'
+import { SlIcon, SlIconButton } from '@shoelace-style/shoelace/dist/react'
+
+export default function ErrorFallback({ resetErrorBoundary }) {
+    return (
+        <div role="alert" style={{ textAlign: 'center' }}>
+            <SlIcon style={{ fontSize: '3em', paddingTop: '1em' }} name="emoji-dizzy" label="error" />
+            <p>Vaya...la hemos cagado</p>
+            <div>re-intentar</div>
+            <SlIconButton
+                name="arrow-clockwise"
+                data-testid="retryBtn"
+                label="reintentar"
+                onClick={resetErrorBoundary}
+            />
+        </div>
+    )
+}
+
+ErrorFallback.propTypes = {
+    resetErrorBoundary: PropTypes.func.isRequired,
+}
