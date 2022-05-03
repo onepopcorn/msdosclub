86c82f9d9e4218315815f291821447db94bbada6
onepopcorn <marc.feliu@gmail.com>
2022-05-03 11:48:53 -0600

    refactor: removed unsued tests after changing folder structure

--- src/components/__tests__/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from '../App';
-
-test('App can me mounted withouht issues', () => {
-    render(<App />);
-    const text = screen.getByText(/ms-dos/i);
-    expect(text).toBeInTheDocument();
-});
