bb1e049544b7985326bff76cd3fec77058d8fad5
onepopcorn <marc.feliu@gmail.com>
2022-05-03 20:51:59 -0600

    test: added tests for single comment

--- src/components/data/Comments/Comment/Comment.test.js
+++ src/components/data/Comments/Comment/Comment.test.js
@@ -1,4 +1,5 @@
-import { render, screen } from '@testing-library/react'
+import { getByRole, render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 
 import Comment from './Comment'
 
@@ -23,10 +24,24 @@ test('Comment should show author & comment content data', () => {
 test('Comment should be marked as connected needed', () => {
     render(<Comment {...testdata} isConnected />)
 
+    // That's an implementation detail that shouldn't be
+    // tested but there's no other way I can think to test this.
     expect(screen.getByRole('listitem')).toHaveClass('connected')
 })
 
-test.skip('Comment should trigger callback when response button is clicked', () => {
+test('Comment should trigger callback when response button is clicked', () => {
+    const responseMock = jest.fn()
+    const { rerender } = render(<Comment {...testdata} onResponse={responseMock} parent={42} />)
+
     // When comment has a parent, parent ID must be passed to callback
-    // When comment has not a parent
+    userEvent.click(screen.getByRole('button'))
+    expect(responseMock).toBeCalledTimes(1)
+    expect(responseMock).toBeCalledWith(42)
+    responseMock.mockReset()
+
+    // When comment has no a parent
+    rerender(<Comment {...testdata} onResponse={responseMock} />)
+    userEvent.click(screen.getByRole('button'))
+    expect(responseMock).toBeCalledTimes(1)
+    expect(responseMock).toBeCalledWith(null)
 })
