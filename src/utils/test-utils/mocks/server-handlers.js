b7163a8af3886a5ecccd57f1cc6e6df533108406
onepopcorn <marc.feliu@gmail.com>
2022-05-17 17:54:22 -0600

    test: added default handlers for posts & comments for tests that don't need them

--- src/utils/test-utils/mocks/server-handlers.js
+++ src/utils/test-utils/mocks/server-handlers.js
@@ -3,6 +3,12 @@ import { rest } from 'msw'
 // Set handlers for common requests that needs to be intercepted here but keep specific
 // handlers close to where they are gonna be used
 const handlers = [
+    rest.get('https://msdos.club/wp-json/wp/v2/posts', (_, res, ctx) => {
+        return res(ctx.json([]))
+    }),
+    rest.get('https://msdos.club/wp-json/wp/v2/comments', (_, res, ctx) => {
+        return res(ctx.json([]))
+    }),
     rest.get('/shoelace/assets/icons/:icon', async (_, res, ctx) => {
         return res(ctx.set('Content-Type', 'image/svg+xml'), ctx.body('<svg xmlns="http://www.w3.org/2000/svg"></svg>'))
     }),
