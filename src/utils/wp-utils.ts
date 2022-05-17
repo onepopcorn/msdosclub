e90bc85531c5001a33fe4d7514c95133c866fbf9
onepopcorn <marc.feliu@gmail.com>
2022-05-17 09:01:56 -0600

    fix: fixed small bug on check node validity & removed unused code

--- src/utils/wp-utils.ts
+++ src/utils/wp-utils.ts
@@ -1,75 +1,57 @@
-import { decode } from 'html-entities';
+import { decode } from 'html-entities'
 
-// const defaultImages = {
-//     thumbnail: {
-//         file: 'default_thumbnail.jpg',
-//         height: 15,
-//         width: 15,
-//         mime_type: "image/jpeg",
-//         source_url: `${process.env.PUBLIC_URL}/default_thumbnail.jpg`,
-//     },
-//     medium: {
-//         file: 'default_medium.jpg',
-//         height: 221,
-//         width: 245,
-//         mime_type: "image/jpeg",
-//         source_url: `${process.env.PUBLIC_URL}/default_medium.jpg`,
-//     },
-//     full: {
-//         file: 'default_full.jpg',
-//         height: 663,
-//         width: 737,
-//         mime_type: "image/jpeg",
-//         source_url: `${process.env.PUBLIC_URL}/default_full.jpg`,
-//     },
-// }
-
+/**
+ * Poor man's cache
+ *
+ */
 const cache = new Map()
 
 /**
  * Utility function remove unwanted nodes from content
- * 
+ *
  */
 function isValidNode(node: HTMLElement, title: string): boolean {
     // Remove node that contains the audio file
-    return !node.querySelector('.powerpress_player')
+    return (
+        !node.querySelector('.powerpress_player') &&
         // Remove node that contains external sources
-        && !node.classList.contains('infopodcast')
+        !node.classList.contains('infopodcast') &&
         // Remove node that contains the post title
-        && node.id !== 'lineaTiempo'
+        node.id !== 'lineaTiempo' &&
         // Remove node with audio file link
-        && !node.innerText.includes('Descargar el episodio')
+        !node.innerText?.includes('Descargar el episodio') &&
         // Remove other possible nodes containing the post title
-        && !(['h2', 'h3'].includes(node.nodeName.toLocaleLowerCase()) && node.innerText.includes(title))
+        !(['h2', 'h3'].includes(node.nodeName.toLocaleLowerCase()) && node.innerText?.includes(title))
+    )
 }
 /**
  * Utility function to clean post content and extract data from it
- * 
+ *
  */
 type AudioSource = {
-    name: string,
-    url: string,
+    name: string
+    url: string
 }
 
 type PodcastData = {
-    content: string,
-    audio: string,
-    sources: AudioSource[],
-    title: string,
-    slug: string,
+    content: string
+    audio: string
+    sources: AudioSource[]
+    title: string
+    slug: string
 }
 
 export const getPodcastdata = (html: string, title: string, slug: string): PodcastData => {
     const parser: DOMParser = new DOMParser()
     const doc: Document = parser.parseFromString(html, 'text/html')
 
-    const audio: HTMLAudioElement|null = doc.querySelector('audio source')
+    const audio: HTMLAudioElement | null = doc.querySelector('audio source')
     const sourcesList: NodeListOf<HTMLAnchorElement> = doc.querySelectorAll('.infopodcast b > a')
     const contents: NodeListOf<HTMLElement> = doc.querySelectorAll('body > *')
 
     const text: string[] = []
     let firstImageFound: boolean = false
-    contents.forEach((node:HTMLElement) => {
+    contents.forEach((node: HTMLElement) => {
         // Discard unwanted nodes
         if (!isValidNode(node, title)) return
 
@@ -83,13 +65,13 @@ export const getPodcastdata = (html: string, title: string, slug: string): Podca
         if (node.nodeName.toLowerCase() === 'figure') node.removeAttribute('style')
 
         // Center images
-        const img: HTMLImageElement| null = node.querySelector('img')
+        const img: HTMLImageElement | null = node.querySelector('img')
         if (img) {
             img.setAttribute('loading', 'lazy')
         }
 
         // External links
-        const links: NodeListOf<HTMLAnchorElement>|null = node.querySelectorAll('a')
+        const links: NodeListOf<HTMLAnchorElement> | null = node.querySelectorAll('a')
         if (links) {
             links.forEach((link: HTMLAnchorElement) => {
                 link.setAttribute('target', '_blank')
@@ -99,21 +81,21 @@ export const getPodcastdata = (html: string, title: string, slug: string): Podca
 
         // Clean any inline style
         let cleaned = node.outerHTML
-                    // remove inline styles
-                    .replace(/style=("|').*("|')/gi, '')
-                    // replace podcast time list with custom links
-                    .replace(/\d{2}:\d{2}:\d{2}/gi, '<a class="audio-link">$&</a>')
-        
+            // remove inline styles
+            .replace(/style=("|').*("|')/gi, '')
+            // replace podcast time list with custom links
+            .replace(/\d{2}:\d{2}:\d{2}/gi, '<a class="audio-link">$&</a>')
+
         text.push(cleaned)
     })
 
     const sources: AudioSource[] = []
     const regex: RegExp = /(apple|ivoox|spotify|google|podimo)/i
-    sourcesList.forEach(node => {
+    sourcesList.forEach((node) => {
         if (!regex.test(node.host)) return
         sources.push({
             name: node.innerText,
-            url: node.href
+            url: node.href,
         })
     })
 
@@ -127,28 +109,29 @@ export const getPodcastdata = (html: string, title: string, slug: string): Podca
 }
 
 /**
- * Utility function to format images 
- * 
+ * Utility function to format images
+ *
  */
 type PostImage = {
-    width: number,
-    height: number,
-    file?: string,
-    source_url: string,
+    width: number
+    height: number
+    file?: string
+    source_url: string
 }
 
 type PostImageData = {
-    thumbnail: PostImage,
-    medium: PostImage,
-    full: PostImage,
-    srcset: string,
-    sizes: string,
-    caption: string,
+    thumbnail: PostImage
+    medium: PostImage
+    full: PostImage
+    srcset: string
+    sizes: string
+    caption: string
 }
 
-export const getPostImages = (embed:any): PostImageData => {
+export const getPostImages = (embed: any): PostImageData => {
     const images = embed['wp:featuredmedia'][0]
-    let { thumbnail, medium, full } : {thumbnail: PostImage, medium: PostImage, full: PostImage} = images.media_details.sizes
+    let { thumbnail, medium, full }: { thumbnail: PostImage; medium: PostImage; full: PostImage } =
+        images.media_details.sizes
 
     const defaultImageValues = {
         source_url: images.source_url,
@@ -157,10 +140,9 @@ export const getPostImages = (embed:any): PostImageData => {
     }
 
     // Ensure there's the 3 image sizes even if they are the same
-    if( !images?.media_details?.sizes?.thumbnail) thumbnail = defaultImageValues
-    if( !images?.media_details?.sizes?.medium) medium = defaultImageValues
-    if( !images?.media_details?.sizes?.full) full = defaultImageValues
-    
+    if (!images?.media_details?.sizes?.thumbnail) thumbnail = defaultImageValues
+    if (!images?.media_details?.sizes?.medium) medium = defaultImageValues
+    if (!images?.media_details?.sizes?.full) full = defaultImageValues
 
     return {
         thumbnail,
@@ -172,28 +154,27 @@ export const getPostImages = (embed:any): PostImageData => {
     }
 }
 
-
 /**
  * Utility function to clean & extract the post data we want
- * 
+ *
  */
 type Post = PodcastData & {
-    id: number,
-    author: string,
-    date: string,
-    images: PostImageData,
+    id: number
+    author: string
+    date: string
+    images: PostImageData
 }
 
-export const processPosts = (posts:any): Post[] =>
-    posts.map((p:any) => {
-        if(cache.has(p.id)) return cache.get(p.id)
-        
+export const processPosts = (posts: any): Post[] =>
+    posts.map((p: any) => {
+        if (cache.has(p.id)) return cache.get(p.id)
+
         const postData = {
             id: p.id,
             author: p._embedded.author[0]?.name,
-            date: (new Date(p.date)).toLocaleDateString(),
+            date: new Date(p.date).toLocaleDateString(),
             ...getPodcastdata(decode(p.content?.rendered), decode(p.title.rendered), p.slug),
-            images: getPostImages(p._embedded)
+            images: getPostImages(p._embedded),
         }
 
         cache.set(p.id, postData)
