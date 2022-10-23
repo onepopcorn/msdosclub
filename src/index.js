e7f200306fcb2b35efa0064384cbeb5135645bb5
onepopcorn <marc.feliu@gmail.com>
2022-10-23 15:24:29 +0200

    refactor: removed shoelace styles from bundle and included in HTML

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import '@shoelace-style/shoelace/dist/themes/light.css'
-import '@shoelace-style/shoelace/dist/themes/dark.css'
 import './index.module.css'
 import App from './components/App'
 
